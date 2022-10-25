7447780f2d9c3a1f72ae03d6613db97980bda3f6
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-10-25 04:23:55 -0500

    experiment: revert blit fix

--- src/LookingGlassXRWebGLLayer.js
+++ src/LookingGlassXRWebGLLayer.js
@@ -219,27 +219,6 @@
        const oldProgram = gl.getParameter(gl.CURRENT_PROGRAM);
        const oldActiveTexture = gl.getParameter(gl.ACTIVE_TEXTURE);
  
-       // Copy to layer framebuffer when targets mismatch
-       const isWebGL2 = gl instanceof _global.WebGL2RenderingContext
-       if (isWebGL2 && oldFramebufferBinding !== this.framebuffer) {
-         gl.bindFramebuffer(gl.READ_FRAMEBUFFER, oldFramebufferBinding);
-         gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, this.framebuffer);
-         gl.blitFramebuffer(
-           0,
-           0,
-           gl.drawingBufferWidth,
-           gl.drawingBufferHeight,
-           0,
-           0,
-           gl.drawingBufferWidth,
-           gl.drawingBufferHeight,
-           gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT,
-           gl.NEAREST,
-         );
-         gl.bindFramebuffer(gl.READ_FRAMEBUFFER, null);
-         gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
-       }
- 
        {
          const oldTextureBinding = gl.getParameter(gl.TEXTURE_BINDING_2D);
          {
