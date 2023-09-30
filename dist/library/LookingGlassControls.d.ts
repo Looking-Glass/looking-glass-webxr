fc0c46fe85ab43a298cc37853991f1ea71efa9f6
Bryan Brown <BryanChristopherBrown@gmail.com>
2023-09-30 14:54:58 -0400

    remove extra ts definitions

--- dist/library/LookingGlassControls.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function initLookingGlassControlGUI(): HTMLDivElement | undefined;
--- dist/library/LookingGlassWindow.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const moveCanvasToWindow: (enabled: boolean, onbeforeunload: any) => void;
