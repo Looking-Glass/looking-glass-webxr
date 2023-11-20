c6ba3c80181195c645372a3b208d4bbf28a88dcd
Bryan Brown <BryanChristopherBrown@gmail.com>
2023-11-20 17:32:32 -0500

    update type defs

--- dist/LookingGlassMediaController.d.ts
+++ dist/LookingGlassMediaController.d.ts
@@ -1,7 +1,1 @@
 export declare function LookingGlassMediaController(screenshotbutton: HTMLButtonElement): Promise<void>;
-declare const resolveWhenIdle: {
-    request: ((callback: IdleRequestCallback, options?: IdleRequestOptions | undefined) => number) & typeof requestIdleCallback;
-    cancel: ((handle: number) => void) & typeof cancelIdleCallback;
-    promise: (num: any) => Promise<unknown>;
-};
-export { resolveWhenIdle };
